5795b3e4e833c1722bb75526d0d359ad572864a3
Gip-Gip <chmithbiz@gmail.com>
2019-03-03 20:57:22 -0500

    Adding functionality

--- src/Chromascript.java
+++ src/Chromascript.java
@@ -9,14 +9,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 
+import javax.imageio.ImageIO;
+
 public class Chromascript
 {
-  private int id[] = new int[36];
-  private final byte idWidth = 18;
+  private int id[];
+  private int idWidth;
   private final byte idHeight = 2;
   private byte data[];
   private int dataSize;
-  private int rgbData[];
+  private long dataLength;
   private int width;
   private int dataWidth;
   private int height;
@@ -27,13 +29,43 @@ public class Chromascript
   private int bMargin;
   private BufferedImage pageImage;
   
-  int[] getRgbData()
+  private class IdSquare
   {
-    calcRgbData();
-    return rgbData;
+    public int pointAX;
+    public int pointAY;
+    public int pointBX;
+    public int pointBY;
+    
+    public double getDiagonal()
+    {
+      return Math.hypot(pointBX-pointAX, pointBY-pointAY);
+    }
+    
+    public double getSide()
+    {
+      return Math.sqrt(Math.pow(getDiagonal(), 2) / 2);
+    }
+    
+    public String toString()
+    {
+      return Integer.toString(pointAX) + " " + Integer.toString(pointAY)
+      + " " + Integer.toString(pointBX) + " " + Integer.toString(pointBY)
+      + " " + Double.toString(getDiagonal())+ " " + Double.toString(getSide());
+    }
   }
   
-  BufferedImage getPageImage()
+  byte[] getData()
+  {
+    calcData();
+    return data;
+  }
+  
+  int[] getRgbData()
+  {
+    return mkRgbData(data);
+  }
+  
+  BufferedImage getPageImage() throws Exception
   {
     calcPageImage();
     return pageImage;
@@ -43,9 +75,15 @@ public class Chromascript
   {
     calcDataSize();
     FileInputStream inStream = new FileInputStream(αfile);
+    dataLength = αfile.length();
     data = new byte[dataSize];
     inStream.read(data);
     inStream.close();
+  }
+  
+  void setPageImage(File αfile) throws Exception
+  {
+    pageImage = ImageIO.read(αfile);
   }
   
   void setWidth(int αwidth)
@@ -90,6 +128,67 @@ public class Chromascript
     setBMargin(marginLength);
   }
   
+  private int meanBrightness(int rgb)
+  {
+    int brightness = (rgb & 0xFF) + ((rgb >>= 8) & 0xFF) + ((rgb >>= 8) & 0xFF);
+    brightness /= 3;
+    
+    return brightness;
+  }
+  
+  void calcData()
+  {
+    IdSquare idOne = new IdSquare();
+    IdSquare idTwo = new IdSquare();
+    
+    int darkest = 0xFFFFFF;
+    int lightest = 0x000000;
+    int range;
+    
+    for(int y = 0; y < pageImage.getHeight(); y++)
+    {
+      for(int x = 0; x < pageImage.getWidth(); x++)
+      {
+        darkest = Math.min(darkest, meanBrightness(pageImage.getRGB(x, y) & 0xFFFFFF));
+        lightest = Math.max(lightest, meanBrightness(pageImage.getRGB(x, y) & 0xFFFFFF));
+      }
+    }
+    
+    range = lightest - darkest;
+    
+    for(int y = 0; y < pageImage.getHeight(); y++)
+    {
+      for(int x = 0; x < pageImage.getWidth(); x++)
+      {
+        if((meanBrightness(pageImage.getRGB(x, y) & 0xFFFFFF)) < (range / 4 + darkest))
+        {
+          idOne.pointAX = x;
+          idOne.pointAY = y;
+        }
+      }
+    }
+    
+    idOne.pointBX = idOne.pointAX;
+    idOne.pointBY = idOne.pointAY;
+    
+    while((meanBrightness(pageImage.getRGB(idOne.pointBX, idOne.pointBY) & 0xFFFFFF)) < (range / 4 + darkest))
+    {
+      if((meanBrightness(pageImage.getRGB(idOne.pointBX + 1, idOne.pointBY) & 0xFFFFFF)) < (range / 4 + darkest))
+        idOne.pointBX++;
+      
+      else if((meanBrightness(pageImage.getRGB(idOne.pointBX, idOne.pointBY + 1) & 0xFFFFFF)) < (range / 4 + darkest))
+        idOne.pointBY++;
+      
+      else
+      {
+        idOne.pointBX ++;
+        idOne.pointBY ++;
+      }
+    }
+    
+    System.out.print(idOne);
+  }
+  
   void calcDataSize()
   {
     calcDataWidth();
@@ -98,9 +197,9 @@ public class Chromascript
     dataSize /= 2;
   }
   
-  void calcRgbData()
+  int[] mkRgbData(byte data[])
   {
-    rgbData = new int[data.length * 2];
+    int rgbData[] = new int[data.length * 2];
     
     for(int ι = 0; ι < data.length; ι++)
     {
@@ -116,6 +215,8 @@ public class Chromascript
       rgbData[ι*2+1] |= ((data[ι] & 0b00000010) > 0 ? 0 : 0x00000040);
       rgbData[ι*2+1] |= ((data[ι] & 0b00000001) > 0 ? 0 : 0x00808080);
     }
+    
+    return rgbData;
   }
   
   void calcDataWidth()
@@ -128,9 +229,16 @@ public class Chromascript
     dataHeight = height - tMargin - bMargin;
   }
   
-  void calcId()
+  void calcId() throws Exception
   {
     int ι = 0;
+    int fileSize[] = 
+        mkRgbData((Long.toString(dataLength) + "\0").getBytes());
+    int fileName[] =
+        mkRgbData((LocalStreams.getInFileName() + "\0").getBytes());
+    
+    idWidth = dataWidth;
+    id = new int[idWidth * idHeight];
     
     id[ι] = 0xFF000000;
     
@@ -143,16 +251,28 @@ public class Chromascript
       id[ι] |= ((16-ι & 0b0001) > 0 ? 0 : 0x00808080);
     }
     
+    for(int character : fileSize)
+    {
+      id[ι++] = character; 
+    }
+    
+    for(int character : fileName)
+    {
+      if(ι < idWidth-2) id[ι++] = character;
+    }
+    
+    if(ι == idWidth-2) throw new Exception("Filename too long!");
+    
+    while(ι < idWidth-1) id[ι++] = 0xFFFFFFFF;
+    
     id[ι] = 0xFF000000;
     
     while((++ι) < id.length) id[ι] = 0xFFFFFFFF;
   }
   
-  void calcPageImage()
+  void calcPageImage() throws Exception
   {
     pageImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-    
-    calcRgbData();
     
     for(int x = 0; x < width; x++)
     {
@@ -168,6 +288,8 @@ public class Chromascript
       (lMargin, tMargin - idHeight, idWidth, idHeight, id, 0, idWidth);
     
     pageImage.setRGB
-      (lMargin, tMargin, dataWidth, dataHeight, rgbData, 0, dataWidth);
+      (lMargin, tMargin, dataWidth, dataHeight, getRgbData(), 0, dataWidth);
+    
+    calcData();
   }
 }
